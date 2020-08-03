683b06e5c3c21efff63401eaea8776b0b0cd8099
cmkadmin <cmkadmin>
2020-08-03 18:12:21 +0000

    Changed notification rule 0

--- conf.d/wato/notifications.mk
+++ conf.d/wato/notifications.mk
@@ -1,3 +1,11 @@
-# Written by Check_MK store (2020-08-03 13:34:03)
+# Written by Check_MK store (2020-08-03 18:12:21)
 
-notification_rules += [{'allow_disable': True, 'description': 'Notify all contacts of a host/service via HTML email', 'disabled': False, 'notify_plugin': ('mail', {}), 'contact_all_with_email': False, 'contact_all': False, 'contact_object': True}]
+notification_rules += [{'allow_disable': True,
+  'comment': u'',
+  'contact_all': False,
+  'contact_all_with_email': False,
+  'contact_object': True,
+  'description': u'Notify all contacts of a host/service via HTML email',
+  'disabled': False,
+  'docu_url': '',
+  'notify_plugin': (u'mail', {})}]
