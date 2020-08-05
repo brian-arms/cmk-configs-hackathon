425f5f3b5e14ab39bbd49db137c318f651529a2b
cmkadmin <cmkadmin>
2020-08-05 23:09:37 +0000

    Modified host briaarms-k8s-rook-2.

--- conf.d/wato/hosts.mk
+++ conf.d/wato/hosts.mk
@@ -3,8 +3,7 @@
 
 all_hosts += ['briaarms-k8s-rook-1', 'briaarms-k8s-rook-2']
 
-host_tags.update({
-  'briaarms-k8s-rook-1': {'address_family': 'ip-v4-only',
+host_tags.update({'briaarms-k8s-rook-1': {'address_family': 'ip-v4-only',
                          'agent': 'cmk-agent',
                          'criticality': 'prod',
                          'ip-v4': 'ip-v4',
@@ -13,23 +12,29 @@ host_tags.update({
                          'site': 'master',
                          'snmp_ds': 'no-snmp',
                          'tcp': 'tcp'},
-  'briaarms-k8s-rook-2': {'piggyback': 'auto-piggyback', 'networking': 'lan', 'agent': 'cmk-agent', 'criticality': 'test', 'snmp_ds': 'no-snmp', 'ip-v4': 'ip-v4', 'site': 'master', 'tcp': 'tcp', 'address_family': 'ip-v4-only'}
-})
-                       
+ 'briaarms-k8s-rook-2': {'address_family': 'ip-v4-only',
+                         'agent': 'cmk-agent',
+                         'criticality': 'test',
+                         'ip-v4': 'ip-v4',
+                         'networking': 'lan',
+                         'piggyback': 'auto-piggyback',
+                         'site': 'foo',
+                         'snmp_ds': 'no-snmp',
+                         'tcp': 'tcp'}})
 
 host_labels.update({})
 
 # Explicit IPv4 addresses
-
-ipaddresses.update({
-  'briaarms-k8s-rook-1': '64.102.178.144',
-  'briaarms-k8s-rook-2': '64.102.189.80'
-})
+ipaddresses.update({'briaarms-k8s-rook-1': '64.102.178.144',
+ 'briaarms-k8s-rook-2': '64.102.189.80'})
 
 # Host attributes (needed for WATO)
 host_attributes.update(
 {'briaarms-k8s-rook-1': {'ipaddress': '64.102.178.144',
                          'meta_data': {'created_at': 1596655235.0,
                                        'created_by': u'cmkadmin'}},
-  'briaarms-k8s-rook-2': {'meta_data': {'created_at': 1596655235.0, 'created_by': u'cmkadmin'}, 'ipaddress': '64.102.189.80', 'tag_criticality': 'test'}
-                                       })
+ 'briaarms-k8s-rook-2': {'ipaddress': '64.102.189.80',
+                         'meta_data': {'created_at': 1596655235.0,
+                                       'created_by': u'cmkadmin'},
+                         'site': 'foo',
+                         'tag_criticality': 'test'}})
